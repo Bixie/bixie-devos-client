f214d5857e32a17f453752138c6df660ba1dacc9
Matthijs Alles <admin@bixie.nl>
2016-09-08 15:35:10 +0200

    cleanup

--- src/DevosClient.php
+++ src/DevosClient.php
@@ -9,9 +9,6 @@ use Bixie\DevosClient\Request\RequestParameters;
 use Bixie\DevosClient\Request\Response;
 use GuzzleHttp\Client;
 use GuzzleHttp\Cookie\CookieJar;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Psr7\Response as GuzzleResponse;
-use GuzzleHttp\Psr7\Uri;
 
 class DevosClient {
 
@@ -26,7 +23,7 @@ class DevosClient {
 	protected $cookieJar;
 
 	/**
-	 * @var Client
+	 * @var RequestApitokenProvider
 	 */
 	protected $apiToken;
 
